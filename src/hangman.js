d8818e17071265f2a697d01748b97412c27e1606
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-20 18:10:49 +0900

    hangman ready for production

---
+++ src/hangman.js
@@ -0,0 +1,60 @@
+class Hangman {
+  constructor(word, guessesRemaining) {
+    this.word = word.toLowerCase().split("");
+    this.guessesRemaining = guessesRemaining;
+    this.guessedLetters = [];
+    this.status = "playing";
+  }
+  get statusMessage() {
+    if (this.status === "playing") {
+      return `Guesses left: ${this.guessesRemaining}`;
+    } else if (this.status === "failed") {
+      return `Nice try! The word was "${this.word.join("")}".`;
+    } else {
+      return "Great work! You guessed the word";
+    }
+  }
+  updateStatus() {
+    const finished = this.word.every(
+      (letter) => this.guessedLetters.includes(letter) || letter === " "
+    );
+
+    if (this.guessesRemaining === 0) {
+      this.status = "failed";
+    } else if (finished) {
+      this.status = "finished";
+    } else {
+      this.status = "playing";
+    }
+  }
+  get puzzle() {
+    let puzzle = "";
+
+    this.word.forEach((letter) => {
+      if (this.guessedLetters.includes(letter) || letter === " ") {
+        puzzle += letter;
+      } else {
+        puzzle += "*";
+      }
+    });
+
+    return puzzle;
+  }
+  makeGuess(guess) {
+    if (this.status === "playing") {
+      guess = guess.toLowerCase();
+      const uniqueGuess = !this.guessedLetters.includes(guess);
+      const badGuess = !this.word.includes(guess);
+
+      if (uniqueGuess) {
+        this.guessedLetters.push(guess);
+      }
+      if (uniqueGuess && badGuess) {
+        this.guessesRemaining--;
+      }
+      this.updateStatus();
+    }
+  }
+}
+
+export { Hangman as default };
---
+++ src/requests.js
@@ -0,0 +1,40 @@
+const getPuzzle = (wordCount) => {
+  return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Unable to fetch the puzzle");
+      }
+    })
+    .then((data) => {
+      return data.puzzle;
+    });
+};
+
+const getCurrentCountry = async () => {
+  const location = await getLocation();
+  const country = await getCountry(location.country);
+  return country;
+};
+
+const getCountry = async (countryCode) => {
+  const response = await fetch("http://restcountries.eu/rest/v2/all");
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.find((country) => country.alpha2Code === countryCode);
+  } else {
+    throw new Error("Unable to fetch the data");
+  }
+};
+
+const getLocation = async () => {
+  const response = await fetch("http://ipinfo.io/json?token=887d846e414900");
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error("Undable to fetch the data");
+  }
+};
+
+export { getPuzzle as default };
